4078cea3b88dc07c3f5dc7ed82b0979254dd8097
evgenyaseev <Yjrbfy70>
2018-01-07 18:33:58 +0300

    Change google-java-format version to 1.5 and change indentation level in Calculator.class to 4.

--- linters/install-linters.sh
+++ linters/install-linters.sh
@@ -10,7 +10,7 @@ GOOGLE_JAVA_FORMAT_HOME=linters/bin/google-java-format
 mkdir -p "${GOOGLE_JAVA_FORMAT_HOME}" && \
 file_path="${GOOGLE_JAVA_FORMAT_HOME}/google-java-format.jar" && \
 wget --output-document="${file_path}" --quiet https://github.com/google/google-java-format/releases/download/google-java-format-1.5/google-java-format-1.5-all-deps.jar && \
-echo "ffee10177bc7b58aeef61466c4f962ea  ${file_path}" | md5sum -c
+echo "f5124dd4ba467fa7d682f483abae34d7  ${file_path}" | md5sum -c
 
 
 echo "Install xmllint"
