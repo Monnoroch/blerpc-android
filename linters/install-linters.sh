d76ca59469fee727e15294e4e09407541fef0a93
evgenyaseev <Yjrbfy70>
2018-01-07 15:02:14 +0300

    Fix discussions.

--- linters/install-linters.sh
+++ linters/install-linters.sh
@@ -19,10 +19,3 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends libxml2-utils=2.9.*
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
-
-echo "Install shellcheck"
-
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends shellcheck=0.3.*
-sudo apt-get clean
-sudo rm -rf /var/lib/apt/lists/*
