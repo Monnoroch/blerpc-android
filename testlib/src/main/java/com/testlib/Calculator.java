7a40702a4dfc8a91349fc8632301eb14604d7382
evgenyaseev <Yjrbfy70>
2018-01-07 15:01:37 +0300

    Fix discussions.

--- testlib/src/main/java/com/testlib/Calculator.java
+++ testlib/src/main/java/com/testlib/Calculator.java
@@ -3,9 +3,7 @@ package com.testlib;
 /** Calculator class. */
 public class Calculator {
 
-    /**
-     * Add up two integers.
-     */
+    /** Add up two integers. */
     public int add(int a, int b) {
         return a + b;
     }
