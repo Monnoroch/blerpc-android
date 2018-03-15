f152e2c4cc1fcd86f80a40c26666dcbd9ed521cb
evgenyaseev <Yjrbfy70>
2018-03-15 23:33:09 +0300

    Change test on change value after from reset to unsubscribe.
    Fixed #15.

--- blerpc/src/main/java/com/blerpc/BleRpcChannel.java
+++ blerpc/src/main/java/com/blerpc/BleRpcChannel.java
@@ -180,6 +180,11 @@ public class BleRpcChannel implements RpcChannel {
     } else if (Arrays.equals(value, DISABLE_NOTIFICATION_VALUE)) {
       SubscriptionCallsGroup subscription = getUnsubscribingSubscription(characteristicUuid);
       subscription.status = SubscriptionStatus.UNSUBSCRIBED;
+      subscription.clearCanceled();
+      if (!subscription.hasAnySubscriber()) {
+        subscriptions.remove(subscription.characteristicUuid);
+        return;
+      }
     } else {
       checkArgument(false, "Unexpected value \"%s\" of the subscription state.", Arrays.toString(value));
     }
