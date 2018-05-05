cf64442064d687d163b110cefa12c5c61ad90d37
aseevei <aseevei@gmail.com>
2018-05-05 21:46:01 +0300

    Fix discussion.

--- reactive-blerpc-test/tests/end-to-end-test.sh
+++ reactive-blerpc-test/tests/end-to-end-test.sh
@@ -9,14 +9,18 @@
 
 generated_service=reactive-blerpc-test/tests/com/device/proto/RxTestService.java
 expected_service=reactive-blerpc-test/tests/test_service_expected_output
-if !(diff "${generated_service}" "${expected_service}") ; then
-   echo -e "${red_color}Generated proto service is not equals to expected service.${default_color}\n"
-   exit 1
+difflines=$(diff "${generated_service}" "${expected_service}")
+if [ $? -ne 0 ]; then
+  echo -e "${red_color}Generated proto service is not equals to expected service.${default_color}\n"
+  echo "$difflines"
+  exit 1
 fi
 
 generated_factory=reactive-blerpc-test/tests/com/blerpc/reactive/BleServiceFactory.java
 expected_factory=reactive-blerpc-test/tests/test_factory_expected_output
-if !(diff "${generated_factory}" "${expected_factory}") ; then
-   echo -e "${red_color}Generated service factory is not equals to expected factory.${default_color}\n"
-   exit 1
+difflines=$(diff "${generated_factory}" "${expected_factory}")
+if [ $? -ne 0 ]; then
+  echo -e "${red_color}Generated service factory is not equals to expected factory.${default_color}\n"
+  echo "$difflines"
+  exit 1
 fi
