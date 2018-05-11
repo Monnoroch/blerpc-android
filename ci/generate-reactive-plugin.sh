8a9193b6bbbcbaec07b931fc4d09a06baeaea1b8
aseevei <aseevei@gmail.com>
2018-05-12 00:37:04 +0300

    Fix generating executable file for testing.

--- ci/generate-reactive-plugin.sh
+++ ci/generate-reactive-plugin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p generated
-cat ci/exe-from-jar.sh reactive-blerpc/build/libs/reactive-blerpc.jar > generated/reactive-blerpc
+cat ci/exe-from-jar.sh reactive-blerpc/build/libs/reactive-blerpc-jdk8.jar > generated/reactive-blerpc
 chmod +x generated/reactive-blerpc
