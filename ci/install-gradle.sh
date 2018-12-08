b486e2f20313a0b17ab9e05ff8f47a07371b50a9
aseevei <aseevei@gmail.com>
2018-12-08 12:06:31 +0300

    Attempt

--- ci/install-gradle.sh
+++ ci/install-gradle.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GRADLE_HOME=/usr/local/gradle
+GRADLE_HOME=/home/travis/gradle
 export PATH=${GRADLE_HOME}:$PATH
 
 mkdir -p /tmp/work
@@ -8,5 +8,5 @@ tmp_gradle=/tmp/work/gradle.zip
 wget --output-document=${tmp_gradle} --quiet https://services.gradle.org/distributions/gradle-4.6-all.zip
 echo "a5c1815080f22e3b3437c32fd6c50758  ${tmp_gradle}" | md5sum -c
 unzip -q ${tmp_gradle} -d /opt/
-ln -s /opt/gradle-4.6/ "${GRADLE_HOME}"
+sudo ln -s /opt/gradle-4.6/ "${GRADLE_HOME}"
 rm -rf /tmp/work
