9e473ea7e38fc7ddd2b0c95f36841577fc2c6e22
aseevei <aseevei@gmail.com>
2018-12-08 15:20:20 +0300

    Attempt

--- ci/create-gradle-wrapper-4.6.sh
+++ ci/create-gradle-wrapper-4.6.sh
@@ -2,7 +2,7 @@
 
 mkdir tmp-project
 cd tmp-project
-gradle init --type basic --dsl groovy --project-name temp
+gradle init --type basic --project-name temp
 gradle wrapper --gradle-version 4.6 --distribution-type all
 cd ..
 mv tmp-project/gradlew ./
