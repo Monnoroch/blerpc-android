bb9d240b36754c3b868cc777e64523357fad2c7e
Vadim K <v.korepanov@auraband.io>
2020-05-15 10:29:13 +0500

    Support for short and long data

--- SwiftBleRpcLibrary/SwiftBleRpcTests/TestBleRpcServiceTest.swift
+++ SwiftBleRpcLibrary/SwiftBleRpcTests/TestBleRpcServiceTest.swift
@@ -13,11 +13,17 @@ class TestBleRpcServiceTest: XCTestCase {
 
     var bleRpcDriverMock: MockBleServiceDriver!
     let characteristicUUID: String = "A0000001-0000-0000-0000-000000000000"
+    var testResponseLongData: Data = Data()
+    var testResponseLong = Device_GetValueResponse.with { response in
+        response.intValue = 94
+    }
     var service: TestService?
     var dispose: Disposable?
 
     override func setUp() {
         super.setUp()
+        testResponseLongData = try! Device_GetValueResponse.bleRpcEncode(proto: testResponseLong)
+        testResponseLongData.append(Data(hex: "longData"))
         bleRpcDriverMock = MockBleServiceDriver(peripheral: CentralManagerSwizzle.instance.peripheral()!)
         service = TestService.init(bleRpcDriverMock)
     }
@@ -49,21 +55,6 @@ class TestBleRpcServiceTest: XCTestCase {
             .first()
 
         XCTAssertEqual(response, testResponse)
-    }
-
-    func testReadInvalidResponse() throws {
-        let testRequest = Device_GetValueRequest()
-
-        stub(bleRpcDriverMock) { stub in
-            when(
-                stub.read(
-                    request: equal(to: try! Device_GetValueRequest.bleRpcEncode(proto: testRequest)),
-                    serviceUUID: equal(to: TestService.TestServiceUUID),
-                    characteristicUUID: equal(to: characteristicUUID)
-                )
-            ).thenReturn(.just(Data()))
-        }
-        XCTAssertThrowsError(try service?.readValue(request: testRequest).toBlocking().first())
     }
 
     func testWrite() throws {
@@ -134,6 +125,87 @@ class TestBleRpcServiceTest: XCTestCase {
                         ]
                     )
             )
+        }
+        let response = try service?.getValueUpdates(request: testRequest)
+            .toBlocking()
+            .toArray()
+        XCTAssertEqual(response, responseArray)
+    }
+
+    func testReadLongData() throws {
+        let testRequest = Device_GetValueRequest()
+        stub(bleRpcDriverMock) { stub in
+            when(
+                stub.read(
+                    request: equal(to: try! Device_GetValueRequest.bleRpcEncode(proto: testRequest)),
+                    serviceUUID: TestService.TestServiceUUID,
+                    characteristicUUID: characteristicUUID
+                )
+            )
+                .thenReturn(.just(testResponseLongData))
+        }
+
+        let response = try service?.readValue(request: testRequest)
+            .toBlocking()
+            .first()
+
+        XCTAssertEqual(response, testResponseLong)
+    }
+
+    func testWriteLongData() throws {
+        let testRequest = Device_SetValueRequest.with { request in
+            request.intValue = 45
+        }
+        let testResponse = Device_SetValueResponse()
+        var longDataResponse = try! Device_SetValueResponse.bleRpcEncode(proto: testResponse)
+        longDataResponse.append(Data(hex: "longData"))
+        stub(bleRpcDriverMock) { stub in
+            when(
+                stub.write(
+                    request: equal(to: try! Device_SetValueRequest.bleRpcEncode(proto: testRequest)),
+                    serviceUUID: TestService.TestServiceUUID,
+                    characteristicUUID: characteristicUUID
+                )
+            )
+                .thenReturn(.just(longDataResponse))
+        }
+
+        let response = try service?.writeValue(request: testRequest)
+            .toBlocking()
+            .first()
+        XCTAssertEqual(response, testResponse)
+    }
+
+    func testSubscribeLongData() throws {
+        let testRequest = Device_GetValueRequest()
+        stub(bleRpcDriverMock) { stub in
+            when(
+                stub.subscribe(
+                    request: equal(to: try! Device_GetValueRequest.bleRpcEncode(proto: testRequest)),
+                    serviceUUID: TestService.TestServiceUUID,
+                    characteristicUUID: characteristicUUID
+                )
+            )
+                .thenReturn(.just(testResponseLongData))
+        }
+        let response = try service?.getValueUpdates(request: testRequest)
+            .toBlocking()
+            .first()
+        XCTAssertEqual(response, testResponseLong)
+    }
+
+    func testSubscribeMultipleValuesLongData() throws {
+        let testRequest = Device_GetValueRequest()
+        let responseArray = [testResponseLong, testResponseLong]
+        stub(bleRpcDriverMock) { stub in
+            when(
+                stub.subscribe(
+                    request: equal(to: try! Device_GetValueRequest.bleRpcEncode(proto: testRequest)),
+                    serviceUUID: TestService.TestServiceUUID,
+                    characteristicUUID: characteristicUUID
+                )
+            )
+                .thenReturn(.from([testResponseLongData, testResponseLongData]))
         }
         let response = try service?.getValueUpdates(request: testRequest)
             .toBlocking()
